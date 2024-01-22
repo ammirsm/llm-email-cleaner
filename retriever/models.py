bb53a7e46ffd750912cb39bbf82496cc0bac37b1
Amir Mehr <amir.saiedmehr@gmail.com>
2024-01-21 19:14:07 -0700

    fix: models.py

--- retriever/models.py
+++ retriever/models.py
@@ -6,11 +6,11 @@ from retriever.constants import EmailAccountTypes
 
 
 class EmailAccount(BaseData):
-    email = models.CharField(max_length=255)
+    email = models.CharField(max_length=5000)
     creds = models.JSONField(null=True, blank=True)
     token = models.JSONField(null=True, blank=True)
     # Field for the showing it is using which service (Gmail, Outlook, etc.)
-    service_type = models.CharField(max_length=255, null=True, blank=True, choices=EmailAccountTypes.get_choices())
+    service_type = models.CharField(max_length=5000, null=True, blank=True, choices=EmailAccountTypes.get_choices())
 
     def get_loader_class(self, results_per_page=10, max_results=10):
         loader = EmailAccountTypes.get(self.service_type)["loader_class"](
@@ -40,15 +40,15 @@ class EmailAccount(BaseData):
 
 
 class EmailMessage(BaseData):
-    external_id = models.CharField(max_length=255, null=True, blank=True)
-    thread_id = models.CharField(max_length=255, null=True)
+    external_id = models.CharField(max_length=5000, null=True, blank=True)
+    thread_id = models.CharField(max_length=5000, null=True)
     snippet = models.TextField(null=True, blank=True)
-    internal_date = models.CharField(max_length=255, null=True, blank=True)
+    internal_date = models.CharField(max_length=5000, null=True, blank=True)
     label_ids = models.JSONField(null=True, blank=True)
-    history_id = models.CharField(max_length=255, null=True, blank=True)
-    subject = models.CharField(max_length=255, null=True, blank=True)
-    sender = models.CharField(max_length=255, null=True, blank=True)
-    recipient = models.CharField(max_length=255, null=True, blank=True)
+    history_id = models.CharField(max_length=5000, null=True, blank=True)
+    subject = models.CharField(max_length=5000, null=True, blank=True)
+    sender = models.CharField(max_length=5000, null=True, blank=True)
+    recipient = models.CharField(max_length=5000, null=True, blank=True)
     copy = models.TextField(null=True, blank=True)
     email_account = models.ForeignKey(
         "EmailAccount", on_delete=models.CASCADE, related_name="email_messages", null=True
