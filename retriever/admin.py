d2413033f6265052a8d40398aaeedb1394c9832a
Amir Mehr <amir.saiedmehr@gmail.com>
2024-01-21 17:18:00 -0700

    add: admin for email account and email message.

--- retriever/admin.py
+++ retriever/admin.py
@@ -1,1 +1,19 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import EmailAccount, EmailMessage
+
+
+class EmailAccountAdmin(admin.ModelAdmin):
+    list_display = ("email", "service_type")
+    search_fields = ("email",)
+
+
+admin.site.register(EmailAccount, EmailAccountAdmin)
+
+
+class EmailMessageAdmin(admin.ModelAdmin):
+    list_display = ("external_id", "subject", "sender", "recipient")
+    search_fields = ("subject", "sender", "recipient")
+
+
+admin.site.register(EmailMessage, EmailMessageAdmin)
